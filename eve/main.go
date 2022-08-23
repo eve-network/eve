d1cf86b7f710f89dabbc7afdf3b4183401e3851a
faddat <jacobgadikian@gmail.com>
2022-08-23 11:13:00 +0000

    pre-wasm tweaks

--- eve/main.go
+++ eve/main.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	simapp "github.com/notional-labs/eve/app"
+	"github.com/notional-labs/eve/app"
 	"github.com/notional-labs/eve/eve/cmd"
 )
 
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
 
-	if err := svrcmd.Execute(rootCmd, "", simapp.DefaultNodeHome); err != nil {
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
 			os.Exit(e.Code)
