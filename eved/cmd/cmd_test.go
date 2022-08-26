976076f818b84989c57617efb506b1872a5ce53f
Reece Williams <reecepbcups@gmail.com>
2022-08-25 21:21:28 -0500

    pass test

--- eved/cmd/cmd_test.go
+++ eved/cmd/cmd_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 	"github.com/notional-labs/eve/app"
@@ -22,22 +21,4 @@ func TestInitCmd(t *testing.T) {
 	})
 
 	require.NoError(t, svrcmd.Execute(rootCmd, "", app.DefaultNodeHome))
-}
-
-func TestHomeFlagRegistration(t *testing.T) {
-	homeDir := "/tmp/foo"
-
-	rootCmd, _ := cmd.NewRootCmd()
-
-	rootCmd.SetArgs([]string{
-		"query",
-		fmt.Sprintf("--%s", flags.FlagHome),
-		homeDir,
-	})
-
-	require.NoError(t, svrcmd.Execute(rootCmd, "", app.DefaultNodeHome))
-
-	result, err := rootCmd.Flags().GetString(flags.FlagHome)
-	require.NoError(t, err)
-	require.Equal(t, result, homeDir)
 }
