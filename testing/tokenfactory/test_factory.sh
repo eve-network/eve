483be97c5e3c12435a28e005083608298013e7df
Reece Williams <reecepbcups@gmail.com>
2022-08-29 21:55:12 -0500

    update factory title

--- testing/tokenfactory/test_factory.sh
+++ testing/tokenfactory/test_factory.sh
@@ -1,7 +1,8 @@
 # TODO: prefix with owner name so no denoms can conflict with namespace? eveaddr/denom
 # TODO: dont allow minting token named eve / ueve?
 
-eved tx tokenfactory create-denom aaa "my denom" 6 100000 false --from eve1 --chain-id eve-t1 --yes -b sync --keyring-backend test
+# = Init token = ($aaa, description, 6 points of percision, 100,000 max supply, maxSupplyNotChangeable, mintable, burnable)
+eved tx tokenfactory create-denom aaa "my denom description" 6 100000 false --from eve1 --chain-id eve-t1 --yes -b sync --keyring-backend test
 eved q tokenfactory show-denom aaa
 # eved q tokenfactory list-denoms
 
