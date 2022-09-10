b23e3521352261b1b38dfa183bef0b393e0b5d92
Jacob Gadikian <jacobgadikian@gmail.com>
2022-09-10 21:05:43 +0700

    fix naming in template

--- app/params/config.go
+++ app/params/config.go
@@ -15,9 +15,9 @@ var (
 	BypassMinFeeMsgTypesKey = "bypass-min-fee-msg-types"
 
 	// customGaiaConfigTemplate defines Gaia's custom application configuration TOML template.
-	customGaiaConfigTemplate = `
+	customEveConfigTemplate = `
 ###############################################################################
-###                        Custom Gaia Configuration                        ###
+###                        Custom Eve Configuration                         ###
 ###############################################################################
 # bypass-min-fee-msg-types defines custom message types the operator may set that
 # will bypass minimum fee checks during CheckTx.
@@ -28,17 +28,17 @@ bypass-min-fee-msg-types = [{{ range .BypassMinFeeMsgTypes }}{{ printf "%q, " . 
 `
 )
 
-// CustomConfigTemplate defines Gaia's custom application configuration TOML
+// CustomConfigTemplate defines Eve's custom application configuration TOML
 // template. It extends the core SDK template.
 func CustomConfigTemplate() string {
 	config := serverconfig.DefaultConfigTemplate
 	lines := strings.Split(config, "\n")
 	// add the Gaia config at the second line of the file
-	lines[2] += customGaiaConfigTemplate
+	lines[2] += customEveConfigTemplate
 	return strings.Join(lines, "\n")
 }
 
-// CustomAppConfig defines Gaia's custom application configuration.
+// CustomAppConfig defines Eve's custom application configuration.
 type CustomAppConfig struct {
 	serverconfig.Config
 
