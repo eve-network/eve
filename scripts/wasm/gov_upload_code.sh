4600867278b100e91654ddc75654c3aeb91fad11
Tuan Tran <tuantran@notional.ventures>
2024-03-27 21:27:51 +0700

    Test(wasmd): add basic e2e tests for wasm (#187)
    * add basic e2e tests for wasm
    
    * fix based on coderabbit review
    
    * add test-wasm to makefile

---
+++ scripts/wasm/gov_upload_code.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+BINARY=build/eved
+
+CHAIN_ID=local-eve
+VAL_KEY=test0
+VOTER=
+VAL=$($BINARY keys show -a $VAL_KEY --keyring-backend test)
+CONTRACT=cw20_base
+PROPOSAL=1
+DENOM=${2:-"ueve"}
+HOME=mytestnet
+
+echo "submit wasm store proposal..."
+if ! $BINARY tx wasm submit-proposal wasm-store scripts/wasm/$CONTRACT.wasm --title "Add $CONTRACT" --summary "Let's upload this contract" --from $VAL_KEY --keyring-backend test --chain-id $CHAIN_ID -y --gas auto --gas-adjustment 1.3 > /dev/null; then
+    echo "Error submitting proposal"
+    exit 1
+fi
+
+
+echo "deposit ueve to proposal..."
+sleep 5
+# $BINARY query gov proposal $PROPOSAL
+$BINARY tx gov deposit $PROPOSAL "40000000000000000000$DENOM" --from $VAL_KEY --keyring-backend test \
+    --chain-id $CHAIN_ID -y -b sync --gas auto --gas-adjustment 1.3 --home $HOME > /dev/null
+
+echo "process to self vote..."
+sleep 5
+$BINARY tx gov vote $PROPOSAL yes --from $VAL_KEY --keyring-backend test \
+    --chain-id $CHAIN_ID -y -b sync --gas auto  --gas-adjustment 1.3  --home $HOME > /dev/null
+
+echo "Waiting for voting periods to finish..."
+COUNTER=0
+while ((COUNTER < 12)); do
+    # Capture output of $BINARY command and extract "status" using jq
+    status=$($BINARY q gov proposal 1 --output=json | jq '.status')
+    sleep 5
+    echo "Current proposal status: $status"
+    # Increment COUNTER using arithmetic expansion
+    ((COUNTER++))
+done
+
+$BINARY query wasm list-code
