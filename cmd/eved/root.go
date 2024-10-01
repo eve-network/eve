92837bf6eb180e0f46b0283fa01613797b6b8478
Jacob Gadikian <jacobgadikian@gmail.com>
2024-10-01 14:40:41 +0800

    Update root.go

--- cmd/eved/root.go
+++ cmd/eved/root.go
@@ -111,7 +111,6 @@ func NewRootCmd() *cobra.Command {
 	// add keyring to autocli opts
 	autoCliOpts := tempApp.AutoCliOpts()
 	initClientCtx, _ = config.ReadFromClientConfig(initClientCtx)
-	autoCliOpts.Keyring, _ = keyring.NewAutoCLIKeyring(initClientCtx.Keyring)
 	autoCliOpts.ClientCtx = initClientCtx
 
 	if err := autoCliOpts.EnhanceRootCommand(rootCmd); err != nil {
