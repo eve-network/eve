3c2922c0088f994c006ecde74a12db022d67e41c
Tugay Emin <tgntrr@gmail.com>
2024-12-06 10:35:50 +0200

    fix: specify default denom

--- cmd/eved/root.go
+++ cmd/eved/root.go
@@ -28,6 +28,8 @@ import (
 // NewRootCmd creates a new root command for wasmd. It is called once in the
 // main function.
 func NewRootCmd() *cobra.Command {
+	sdk.DefaultBondDenom = "ueve"
+
 	cfg := sdk.GetConfig()
 	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
 	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
